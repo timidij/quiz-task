baa9ad1a8677d3a4201de2ba8f190fdbc5d5c8c8
unknown <timidiodubo@gmail.com>
2024-08-05 19:50:15 +0100

    uploaded now

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ questions.forEach((question,index)=>{
                 label.style.color = 'red';
               }
             } else if (radio.value === correctAnswer) {
-              label.style.color = 'green'; // Correct anbswer, not selected
+              label.style.color = 'green'; // Correct anbsw er, not selected
               
             }
           });
